f98dc253f751d5ca92bf581965532757aa7b5e3e
Soh1121 <satou.shg@gmail.com>
2021-12-10 15:25:07 +0900

    [add]異なる条件の集計を１つのSQLで行う方法を学習

---
+++ src/Ch01/createPopTbl2.sql
@@ -0,0 +1,27 @@
+CREATE TABLE PopTbl2
+(pref_name	VARCHAR(5)	NOT NULL,
+ sex		CHAR(1)		NOT NULL,
+ population	INTEGER		NOT NULL,
+ PRIMARY KEY (pref_name, sex)
+);
+
+BEGIN TRANSACTION;
+
+INSERT INTO PopTbl2 VALUES ('徳島', '1', 60);
+INSERT INTO PopTbl2 VALUES ('徳島', '2', 40);
+INSERT INTO PopTbl2 VALUES ('香川', '1', 100);
+INSERT INTO PopTbl2 VALUES ('香川', '2', 100);
+INSERT INTO PopTbl2 VALUES ('愛媛', '1', 100);
+INSERT INTO PopTbl2 VALUES ('愛媛', '2', 50);
+INSERT INTO PopTbl2 VALUES ('高知', '1', 100);
+INSERT INTO PopTbl2 VALUES ('高知', '2', 100);
+INSERT INTO PopTbl2 VALUES ('福岡', '1', 100);
+INSERT INTO PopTbl2 VALUES ('福岡', '2', 200);
+INSERT INTO PopTbl2 VALUES ('佐賀', '1', 20);
+INSERT INTO PopTbl2 VALUES ('佐賀', '2', 80);
+INSERT INTO PopTbl2 VALUES ('長崎', '1', 125);
+INSERT INTO PopTbl2 VALUES ('長崎', '2', 125);
+INSERT INTO PopTbl2 VALUES ('東京', '1', 250);
+INSERT INTO PopTbl2 VALUES ('東京', '2', 150);
+
+COMMIT;

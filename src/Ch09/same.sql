734eeb07d59fc383b9cc167ac6063e310a2fa6ef
Soh1121 <satou.shg@gmail.com>
2021-12-18 17:28:15 +0900

    [add]テーブル同士のコンペア--集合の相等性チェック[応用編]を学習

--- src/Ch09/same.sql
+++ src/Ch09/same.sql
@@ -4,3 +4,14 @@ SELECT COUNT(*) AS row_cnt
 	 UNION
 	SELECT *
 	  FROM tbl_B) TMP;
+
+SELECT CASE WHEN COUNT(*) = 0
+            THEN '等しい'
+	    ELSE '異なる' END AS result
+  FROM ((SELECT * FROM tbl_A
+          UNION
+	 SELECT * FROM tbl_B)
+        EXCEPT
+	(SELECT * FROM tbl_A
+	 INTERSECT
+	 SELECT * FROM tbl_B)) TMP;
